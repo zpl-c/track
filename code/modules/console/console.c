bf7b183af9963f4e6dcc3cedc9ffa8a5d48e30b3
Dominik Madarász <zaklaus@outlook.com>
2021-06-26 22:22:12 +0200

    add datetime to console log

--- code/modules/console/console.c
+++ code/modules/console/console.c
@@ -1,17 +1,22 @@
 #include "console.h"
 #include "track.h"
 #include <stdio.h>
+#include <time.h>
 
 #define ZPL_NANO
 #include "zpl.h"
 
 TRACK_EVENT_PROC(console__event_handler) {
-    (void)user_data;
-    // TODO(zaklaus): convert timestamp to date-time
-    printf("[%lld] %s@'%s' -- \"%s\"\n", zpl_time_win32_to_unix(zpl_time_utc_ms()), user_id, event_id, data);
+    if (!user_data)
+        user_data = "track_module_console";
+    char datetime[80];
+    time_t now = time(0);
+    struct tm *tm_now = localtime(&now);
+    strftime(datetime, 80, "%c", tm_now);
+    printf("[%s : %s] %s@'%s' -- \"%s\"\n", (char const *)user_data, datetime, user_id, event_id, data);
     return 0;
 }
 
-int track_module_console_register(void) {
-    return track_module_register(console__event_handler, 0, 0);
+int track_module_console_register(char const *prefix) {
+    return track_module_register(console__event_handler, 0, (void*)prefix);
 }
--- code/modules/console/console.h
+++ code/modules/console/console.h
@@ -1,4 +1,4 @@
 #pragma once
 
-int track_module_console_register(void);
+int track_module_console_register(char const *prefix);
 
