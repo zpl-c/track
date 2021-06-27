f79b5feebc4962f4ff5edae76fc683319ffc35a3
Dominik Madarász <zaklaus@outlook.com>
2021-06-27 18:23:42 +0200

    Place CPM to cmake dir

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -21,25 +21,7 @@ endmacro(use_cxx11)
 macro(setup_curl)
     # Prefer static libs over shared lib
     LIST(REVERSE CMAKE_FIND_LIBRARY_SUFFIXES)
-    set(CPM_DOWNLOAD_VERSION 0.32.0)
-
-    if(CPM_SOURCE_CACHE)
-    set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-    elseif(DEFINED ENV{CPM_SOURCE_CACHE})
-    set(CPM_DOWNLOAD_LOCATION "$ENV{CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-    else()
-    set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
-    endif()
-
-    if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
-    message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
-    file(DOWNLOAD
-        https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-        ${CPM_DOWNLOAD_LOCATION}
-    )
-    endif()
-
-    include(${CPM_DOWNLOAD_LOCATION})
+    include(../cmake/CPM.cmake)
 
     OPTION(ENABLE_OPENSSL_FROM_SOURCE "BUILD OPENSSL FROM SOURCE AT CONFIGURE TIME" ON)
 
