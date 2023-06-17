ee95169cd378146aa26ee5f5af26335542bb62af
Dominik Madarász <zaklaus@outlook.com>
2023-06-17 13:25:48 +0200

    demo: add extra event prop

--- code/apps/examples/demo.c
+++ code/apps/examples/demo.c
@@ -17,7 +17,7 @@ int main() {
     CHECK(track_init("127.0.0.1", "8200"));
 
     CHECK(track_ident("0421", "{\\\"demoId\\\": 42, \\\"name\\\": \\\"Jane Doe\\\"}"));
-    CHECK(track_event("open_demo_app", "0421", "{\\\"foo\\\": 123}"));
+    CHECK(track_event("open_demo_app", "0421", "{\\\"foo\\\": 123, \\\"open_timestamp\\\": 123893893}"));
     CHECK(track_event("do_something", "0421", "{\\\"foo\\\": \\\"bar\\\"}"));
     CHECK(track_group("0421", "abc", "{\\\"demoId\\\": 42, \\\"name\\\": \\\"Jane Doe\\\"}"));
 
