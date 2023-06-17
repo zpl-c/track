69dd49dee57157f24edd92642a87301087189337
Dominik Madarász <zaklaus@outlook.com>
2023-06-17 11:47:18 +0200

    js: use spaces

--- trackjs/demo.js
+++ trackjs/demo.js
@@ -1,26 +1,26 @@
 const track = require('./index.js')
 
-const host = process.env.HOST 	   || null
-const port = process.env.PORT 	   || 8200
+const host = process.env.HOST      || null
+const port = process.env.PORT      || 8200
 const prot = process.env.PROT_KIND || 'udp6'
 
 track.init(prot)
 
 track.on('event', ({userId, event, properties}) => {
-	console.log(`Event: ${event} from [${userId}]: ${properties}`)
+    console.log(`Event: ${event} from [${userId}]: ${properties}`)
 })
 
 track.on('ident', ({userId, traits}) => {
-	console.log(`User: ${userId} with traits: ${traits}`)
+    console.log(`User: ${userId} with traits: ${traits}`)
 })
 
 track.on('group', ({userId, groupId, traits}) => {
-	console.log(`User: ${userId}, group: ${groupId} with traits: ${traits}`)
+    console.log(`User: ${userId}, group: ${groupId} with traits: ${traits}`)
 })
 
 track.on('error', (err, msg) => {
-	console.error(err)
-	if (msg) console.error(msg)
+    console.error(err)
+    if (msg) console.error(msg)
 })
 
 track.bind(port, host)
--- trackjs/index.js
+++ trackjs/index.js
@@ -4,62 +4,62 @@
 const dgram = require('dgram')
 
 module.exports = {
-	init: (type='udp6', ipv6_only=false) => {
-		module.callbacks = {}
-		module.sock = dgram.createSocket({type: type, ipv6Only: ipv6_only})
+    init: (type='udp6', ipv6_only=false) => {
+        module.callbacks = {}
+        module.sock = dgram.createSocket({type: type, ipv6Only: ipv6_only})
 
-		module.sock.on('error', err => {
-			if (module.cb_error) module.cb_error(err)
-		})
+        module.sock.on('error', err => {
+            if (module.cb_error) module.cb_error(err)
+        })
 
-		module.sock.on('message', (rmsg, rinfo) => {
-			const msg = rmsg.toString()
-			if (module.callbacks["log"]) module.callbacks["log"](msg, rinfo)
+        module.sock.on('message', (rmsg, rinfo) => {
+            const msg = rmsg.toString()
+            if (module.callbacks["log"]) module.callbacks["log"](msg, rinfo)
 
-			try {
-				const payload = JSON.parse(msg)
-				const numFields = Object.keys(payload).length
+            try {
+                const payload = JSON.parse(msg)
+                const numFields = Object.keys(payload).length
 
-				// determine which callback to execute
-				/**/if (numFields == 3 
-						&& payload.hasOwnProperty('userId') 
-						&& payload.hasOwnProperty('event') 
-						&& payload.hasOwnProperty('properties')) {
-					if (module.callbacks["event"]) module.callbacks["event"](payload)
-				}
-				else if (numFields == 3 
-						&& payload.hasOwnProperty('userId') 
-						&& payload.hasOwnProperty('groupId') 
-						&& payload.hasOwnProperty('traits')) {
-					if (module.callbacks["group"]) module.callbacks["group"](payload)
-				}
-				else if (numFields == 2 
-						&& payload.hasOwnProperty('userId') 
-						&& payload.hasOwnProperty('traits')) {
-					if (module.callbacks["ident"]) module.callbacks["ident"](payload)
-				} else {
-					if (module.callbacks["unknown"]) module.callbacks["unknown"](payload)
-				}
-			}
-			catch (err) {
-				if (module.callbacks["error"]) module.callbacks["error"](err, msg)
-			}
-		})
-	},
+                // determine which callback to execute
+                /**/if (numFields == 3 
+                        && payload.hasOwnProperty('userId') 
+                        && payload.hasOwnProperty('event') 
+                        && payload.hasOwnProperty('properties')) {
+                    if (module.callbacks["event"]) module.callbacks["event"](payload)
+                }
+                else if (numFields == 3 
+                        && payload.hasOwnProperty('userId') 
+                        && payload.hasOwnProperty('groupId') 
+                        && payload.hasOwnProperty('traits')) {
+                    if (module.callbacks["group"]) module.callbacks["group"](payload)
+                }
+                else if (numFields == 2 
+                        && payload.hasOwnProperty('userId') 
+                        && payload.hasOwnProperty('traits')) {
+                    if (module.callbacks["ident"]) module.callbacks["ident"](payload)
+                } else {
+                    if (module.callbacks["unknown"]) module.callbacks["unknown"](payload)
+                }
+            }
+            catch (err) {
+                if (module.callbacks["error"]) module.callbacks["error"](err, msg)
+            }
+        })
+    },
 
-	stop: () => {
-		module.sock.close()
-	},
+    stop: () => {
+        module.sock.close()
+    },
 
-	bind: (port, host) => {
-		if (module.sock === null)
-			throw "Make sure to call init first!"
-		module.sock.bind(port, host)
-	},
+    bind: (port, host) => {
+        if (module.sock === null)
+            throw "Make sure to call init first!"
+        module.sock.bind(port, host)
+    },
 
-	on: (kind, cb) => {
-		if (module.sock === null)
-			throw "Make sure to call init first!"
-		module.callbacks[kind] = cb
-	}
+    on: (kind, cb) => {
+        if (module.sock === null)
+            throw "Make sure to call init first!"
+        module.callbacks[kind] = cb
+    }
 }
