a74372a7fafee882f24466d9ea5d0fdf07edc3ba
Dominik Madarász <zaklaus@outlook.com>
2023-06-17 12:33:31 +0200

    demo: add more events

--- code/apps/examples/demo.c
+++ code/apps/examples/demo.c
@@ -15,15 +15,11 @@ int err = 0;
 
 int main() {
     CHECK(track_init("127.0.0.1", "8200"));
-    CHECK(track_ident("0421", "demo,quick"));
-    CHECK(track_event("open_demo_app", "0421", "1234"));
-    CHECK(track_group("0421", "abc", "demo,quick"));
 
-    // for (int i = 0; i < 999; i++) {
-    //     static char buf[4]={0};
-    //     snprintf(buf, 4, "%d", i);
-    //     CHECK(track_event("demo_counter", "0421", buf));
-    // }
+    CHECK(track_ident("0421", "demo,quick"));
+    CHECK(track_event("open_demo_app", "0421", "{\\\"foo\\\": 123}"));
+    CHECK(track_event("do_something", "0421", "{\\\"foo\\\": \\\"bar\\\"}"));
+    CHECK(track_group("0421", "abc", "demo,quick"));
 
     CHECK(track_destroy());
     return 0;
