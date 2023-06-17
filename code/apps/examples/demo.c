2827276a1c97772b07910acea6d4f5713220b1b8
Dominik Madarász <zaklaus@outlook.com>
2023-06-17 12:39:04 +0200

    demo: improve code

--- code/apps/examples/demo.c
+++ code/apps/examples/demo.c
@@ -16,10 +16,10 @@ int err = 0;
 int main() {
     CHECK(track_init("127.0.0.1", "8200"));
 
-    CHECK(track_ident("0421", "demo,quick"));
+    CHECK(track_ident("0421", "{\\\"demoId\\\": 42, \\\"name\\\": \\\"Jane Doe\\\"}"));
     CHECK(track_event("open_demo_app", "0421", "{\\\"foo\\\": 123}"));
     CHECK(track_event("do_something", "0421", "{\\\"foo\\\": \\\"bar\\\"}"));
-    CHECK(track_group("0421", "abc", "demo,quick"));
+    CHECK(track_group("0421", "abc", "{\\\"demoId\\\": 42, \\\"name\\\": \\\"Jane Doe\\\"}"));
 
     CHECK(track_destroy());
     return 0;
